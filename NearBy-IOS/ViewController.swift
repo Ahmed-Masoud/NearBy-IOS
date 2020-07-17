574469de8dadfc5aa086cdfb5adc19e77ad8c55e
Ahmed-Masoud <ahmed_ehab200@hotmail.com>
2020-07-17 05:07:32 +0200

    [ADD] project files

---
+++ NearBy-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearBy-IOS
+//
+//  Created by Ahmed masoud on 7/17/20.
+//  Copyright © 2020 Ahmed Masoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
